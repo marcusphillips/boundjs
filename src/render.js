e23fb2d0965a9eeb20de8f26d9f84c26c224ad61
Nathan Houle <ndhoule@gmail.com>
2012-12-11 00:52:31 -0800

    Merged upstream patch, added new proxy test

--- src/render.js
+++ src/render.js
@@ -7,26 +7,9 @@
   };
 
   bound.render = function($node, scope){
-    scope.get = function(key) {
-      if (bound.Context.current !== null) {
-        scope.listeners.push(bound.Context.current);
-      } 
-      return scope[key];
-    };
-    scope.changed = function(key) {
-
-    };
-    scope.ctrl = function(){
-      for ( i = 0; i < scope.listeners.length; i++) {
-        scope.listeners[i].invalidate();
-      }
-    };
-
-    scope.listeners = scope.listeners || [];
-
-    bound.autorun(function() {
-      var key = $node.attr("contents");
-      $node.html(key in scope ? scope.get(key) : global[key]);
+    bound.autorun(function(){
+      var directive = $node.attr("contents");
+      $node.html(bound(scope).ctrl('has', directive) ? scope.ctrl('get', directive) : bound('global', 'get', directive));
     });
   };
 
