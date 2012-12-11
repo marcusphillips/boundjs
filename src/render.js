e0ca17ace98c452781a2ced5ceec9fbd8513f248
Marcus Phillips <github.com@marcusphillips.com>
2012-12-10 20:34:58 -0800

    moved implementation to hints

--- src/render.js
+++ src/render.js
@@ -7,10 +7,6 @@
   };
 
   bound.render = function($node, context){
-    bound.autorun(function(){
-      var directive = $node.attr("contents");
-      $node.html(bound(context).ctrl('has', directive) ? context.ctrl('get', directive) : bound('global', 'get', directive));
-    });
   };
 
 }());
