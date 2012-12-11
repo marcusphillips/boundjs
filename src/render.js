6de48a3531f78629289ffda8df53aa2d9576ff95
Nathan Houle <ndhoule@gmail.com>
2012-12-11 00:37:58 -0800

    Rendering and registering listeners now working

--- src/render.js
+++ src/render.js
@@ -1,12 +1,33 @@
 (function(){
   var global = this;
 
-  $.fn.boundRender = function(context){
-    bound.render(this, context);
+  $.fn.boundRender = function(scope){
+    bound.render(this, scope);
     return this;
   };
 
-  bound.render = function($node, context){
+  bound.render = function($node, scope){
+    scope.get = function(key) {
+      if (bound.Context.current !== null) {
+        scope.listeners.push(bound.Context.current);
+      } 
+      return scope[key];
+    };
+    scope.changed = function(key) {
+
+    };
+    scope.ctrl = function(){
+      for ( i = 0; i < scope.listeners.length; i++) {
+        scope.listeners[i].invalidate();
+      }
+    };
+
+    scope.listeners = scope.listeners || [];
+
+    bound.autorun(function() {
+      var key = $node.attr("contents");
+      $node.html(key in scope ? scope.get(key) : global[key]);
+    });
   };
 
 }());
