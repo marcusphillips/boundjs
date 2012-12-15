b3bc13a67a7c386dd58854a4596137583ee62a5c
Catalyst Students <accounts@catalystclass.com>
2012-12-15 01:35:26 -0800

    Extended underscore to include _.create

--- src/undermore.js
+++ src/undermore.js
@@ -4,4 +4,10 @@
       throw new Error(text);
     }
   };
+  _.create = function (o) {
+    _.raiseIf(arguments.length > 1, 'Object.create implementation only accepts the first parameter.');
+    function F() {}
+    F.prototype = o;
+    return new F();
+  }
 }());
