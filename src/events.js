fa4e53b242372787966163634fe677b6806f05f2
June Jung <jeongjueun@gmail.com>
2012-12-15 00:05:24 -0800

    Fixed indentation

--- src/events.js
+++ src/events.js
@@ -1,10 +1,13 @@
 var mixEvents = function(target){
-	var eventHash = {};
-	target.on = function(event , func){
-    eventHash[event] = func
-	};
-	target.trigger = function(event){
-    eventHash[event]()
-	};
+  var eventHash = {};
+
+  target.on = function(event , func){
+    eventHash[event] = func;
+  };
+
+  target.trigger = function(event){
+    eventHash[event]();
+  };
+
   return target;
 };
