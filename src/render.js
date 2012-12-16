4635c9d6823879751d13c826cb72eecbe3178fdc
Mark Rossetti <mark.a.rossetti@gmail.com>
2012-12-15 23:32:19 -0800

    Further refactor of directive processors

--- src/render.js
+++ src/render.js
@@ -9,23 +9,25 @@
   bound.render = function($node, scope){
     bound.autorun(function(){
 
-      handleContentsDirectives($node, scope);
-      handleAttrDirectives($node, scope);
+      _.each(directiveProcessors, function(processor){
+        processor($node, scope);
+      });
 
     });
   };
 
-  var handleContentsDirectives = function($node, scope) {
-    var directive = $node.attr("contents");
-    $node.html(bound.proxy(scope).bound('has', directive) ? scope.bound('get', directive) : bound('get', directive));
-  };
-
-  var handleAttrDirectives = function($node, scope) {
-    _.each($node[0].attributes, function(attribute){
-      if((/^attr/).test(attribute.name)) {
-        $node.attr((attribute.name).slice("attr-".length), scope[attribute.value]);
-      }
-    });
+  var directiveProcessors = {
+    contents: function($node, scope) {
+      var directive = $node.attr("contents");
+      $node.html(bound.proxy(scope).bound('has', directive) ? scope.bound('get', directive) : bound('get', directive));
+    },
+    attr: function($node, scope) {
+      _.each($node[0].attributes, function(attribute){
+        if((/^attr/).test(attribute.name)) {
+          $node.attr((attribute.name).slice("attr-".length), scope[attribute.value]);
+        }
+      });
+    }
   };
 
 }());
