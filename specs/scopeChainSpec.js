b61358ca1098d45372c20b31cf88614186611231
June Jung <jeongjueun@gmail.com>
2012-12-15 20:08:44 -0800

    passed one test from scope chain

--- specs/scopeChainSpec.js
+++ specs/scopeChainSpec.js
@@ -1,12 +1,12 @@
 describe('scope chains', function(){
   var global = this;
 
-  xit('should provide a scope chain maker function', function(){
+  it('should provide a scope chain maker function', function(){
     expect(bound.topScopeChain).toEqual(jasmine.any(Object));
     expect(bound.topScopeChain.extend).toEqual(jasmine.any(Function));
   });
 
-  xit('should allow look ups in the global scope from the top level scope chain', function(){
+  it('should allow look ups in the global scope from the top level scope chain', function(){
     global.present = 3;
     expect(bound.topScopeChain.get('present')).toEqual(3);
     expect(bound.topScopeChain.get('absent')).toEqual(undefined);
