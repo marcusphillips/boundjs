5d6b6fe7e3eff6023651b939d3086450094d51af
June Jung <jeongjueun@gmail.com>
2012-12-16 23:29:47 -0800

    allow lookups for string literals in qoutes(need refactor it)

--- src/scopeChain.js
+++ src/scopeChain.js
@@ -4,9 +4,14 @@
   bound.topScopeChain.get = function(arg){
   	if( /\d+/.test(arg) ){
   	  return parseInt(arg);
-  	}
-  	else{
-  	  return window[arg]
+  	}else {
+      if(window[arg]){
+        return window[arg];
+      }else if(/'[^']*'|"[^"]*"/.test(arg)){
+        return arg.slice(1, arg.length - 1);
+      }
   	}
   };
 }());
+
+//
