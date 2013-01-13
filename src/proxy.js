d1480184e65debcc9f06efa03427c7f20a18410c
Nathan Houle <nathan@nathanhoule.com>
2013-01-12 20:13:37 -0800

    Fixed merge error

--- src/proxy.js
+++ src/proxy.js
@@ -18,11 +18,7 @@
     };
     _.extend(target, {
       _dependentContextSets: {},
-      bound: function(commandName) {
-        _.raiseIf(this !== target, "cannot call bound on foreign objects.");
-        _.raiseIf(target.bound === undefined, "cannot call bound on objects that lack a bound method.");
-        return proxy[commandName].apply(proxy, _.toArray(arguments).slice(1));
-      }
+      bound: boundMethod
     });
     target.bound.prototype = boundMethodFlag;
   };
