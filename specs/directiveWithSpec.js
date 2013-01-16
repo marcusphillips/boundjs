60565ae0741d37761fb05c207227c83ad563a980
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-01-15 22:42:52 -0800

    Small changes to directiveWithSpec tests

--- specs/directiveWithSpec.js
+++ specs/directiveWithSpec.js
@@ -9,13 +9,22 @@ describe('with', function() {
   xit('should fall back to other namespaces', function() {
     expect($email.render({
       name: 'hello',
-      sender: {
-      }
+      sender: {}
     }).find('.name').html()).toEqual('hello');
   });
 
   it('should ignore fallback namespaces when the value is present in the specified namespace', function() {
-    expect($email.render(message).find('.name').html()).toEqual('alice');
+    expect($email.render({
+      name: 'hello',
+      sender: {
+        name: 'alice'
+      }
+    }).find('.name').html()).toEqual('alice');
+  });
+
+  xit('does not render directives nested under a with directive that resolve to a non-object', function(){
+    expect($email.render({name: 'irrelevant'}).find('.name').html()).toEqual('');
+    expect(bound.getDirectiveRenderCount()).toEqual(1);
   });
 
 });
