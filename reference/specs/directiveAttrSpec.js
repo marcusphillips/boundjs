7fd91929793ad24ff447898cd6046241567ffd26
Nathan Houle <nathan@nathanhoule.com>
2013-01-19 19:35:11 -0800

    Removed legacy jasmine files

--- reference/specs/directiveAttrSpec.js
+++
@@ -1,9 +0,0 @@
-describe('attr', function(){
-
-  it('should handle the attr directive, by adding an attribute with the supplied attribute name and value', function(){
-    var $node = $('<div bound-attr-foo="name"></div>');
-    $node.render(alice);
-    expect($node.attr('foo')).toEqual('alice');
-  });
-
-});
